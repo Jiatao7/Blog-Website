183aed5e4178d2a126218c051dc03b5afd1a7b6b
Jiatao Geng <jiataogeng7@gmail.com>
2024-12-27 11:10:15 -0500

    add styling

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express")
 //Initialize app
 const app = express()
 app.listen(3000)
-
 app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
     console.log(req.url)
