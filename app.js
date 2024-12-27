8c76b76b8370a5bcb9e929666f04700ec0157bbd
Jiatao Geng <jiataogeng7@gmail.com>
2024-12-27 12:45:28 -0500

    add blog model

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 //Modules
 const express = require("express")
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //Initialize app
 const app = express()
-app.listen(3000)
 app.set('view engine', 'ejs')
-app.use(express.static('public'));
 
+//Connect to MongoDB
+const dbURI = "mongodb+srv://Jiatao7:ZZtZX1LJLu4Xh5ou@cluster0.9ulw8.mongodb.net/new-blog-website?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(dbURI)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+//Middleware
+app.use(express.static('public'));
 app.use((req, res, next) => {
     console.log(req.url)
     next()
 })
 
+//Routing
 app.get("/", (req, res) => {
     res.render('index')
 })
