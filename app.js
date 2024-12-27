1598f8aff4267099c2a058f01d781b8d9430505a
Jiatao Geng <jiataogeng7@gmail.com>
2024-12-27 10:41:44 -0500

    create homepage

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+//Modules
 const express = require("express")
 
+//Initialize app
 const app = express()
 app.listen(3000)
+
 app.set('view engine', 'ejs')
+
+app.use((req, res, next) => {
+    console.log(req.url)
+    next()
+})
 
 app.get("/", (req, res) => {
     res.render('index')
